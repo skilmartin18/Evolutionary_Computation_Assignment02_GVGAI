bc49f8de92639980db05e25e8b1d2866d37b03e8
Seb Kilmartin <sebastian.kilmartin@gmail.com>
2021-09-23 23:26:48 +0930

    fuck you fouad let me sleep

---
+++ evo_exercises/Ex4_diy_GA/individual.java
@@ -0,0 +1,40 @@
+package evo_exercises.Ex4_diy_GA;
+
+import tracks.singlePlayer.tools.Heuristics.SimpleStateHeuristic;
+import core.game.StateObservation;
+import core.player.AbstractPlayer;
+import tools.ElapsedCpuTimer;
+import tools.Utils;
+import ontology.*;
+import ontology.Types.ACTIONS;
+import java.util.ArrayList;
+import java.util.Random;
+import core.game.StateObservation;
+
+public class individual {
+
+    ArrayList<Types.ACTIONS> genotype;
+    Random rand = new Random();
+
+    public individual(StateObservation StateObs, int genotype_size)
+    {
+        genotype = create_individual(StateObs,genotype_size);
+    }
+    
+    // create individual based on stateObs, creates individual of size set at beginning
+    public ArrayList<Types.ACTIONS> create_individual(StateObservation stateObs, int genotype_size)
+    {
+        // get available actions (maybe move this out to increase performance)
+        ArrayList<Types.ACTIONS> actions = stateObs.getAvailableActions();
+        int available_actions = actions.size();
+        ArrayList<Types.ACTIONS> individual = new ArrayList<Types.ACTIONS>();
+
+        // add actions from the list of available actions to individual
+        for(int i = 0; i < genotype_size; i++)
+        {
+            individual.add( actions.get(rand.nextInt(available_actions)) );
+        }
+
+        return individual;
+    }
+}
