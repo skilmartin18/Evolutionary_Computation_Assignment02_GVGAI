c60d657b769259a6e5cbd9cf1c34c47f974f9265
Seb Kilmartin <sebastian.kilmartin@gmail.com>
2021-09-24 14:13:03 +0930

    ex4

--- evo_exercises/Ex4_diy_GA/individual.java
+++ evo_exercises/Ex4_diy_GA/individual.java
@@ -15,10 +15,16 @@ public class individual {
 
     ArrayList<Types.ACTIONS> genotype;
     Random rand = new Random();
+    double fitness = 0;
 
     public individual(StateObservation StateObs, int genotype_size)
     {
         genotype = create_individual(StateObs,genotype_size);
+    }
+
+    public individual(ArrayList<Types.ACTIONS> _genotype)
+    {
+        genotype = _genotype;
     }
     
     // create individual based on stateObs, creates individual of size set at beginning
