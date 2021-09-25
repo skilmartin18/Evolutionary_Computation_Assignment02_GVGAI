080d5e1560d0ad4b9d6357fa2c054ff4a0bd0f3f
Seb Kilmartin <sebastian.kilmartin@gmail.com>
2021-09-25 23:09:53 +0930

    shid

--- src/tracks/singlePlayer/tools/Heuristics/SimplestHeuristic.java
+++ src/tracks/singlePlayer/tools/Heuristics/SimplestHeuristic.java
@@ -25,18 +25,18 @@ public class SimplestHeuristic {
         double score = rawScore; 
         double closestResource = Double.POSITIVE_INFINITY;
         double closestNPC = Double.POSITIVE_INFINITY;
-        // double closestImmovable = Double.POSITIVE_INFINITY;
+        double closestImmovable = Double.POSITIVE_INFINITY;
 
         // calculate closest npc or resource, 1/(distance/100) is added to score
         // plan to allow this to take different parameters depending on game, may not be possible
         ArrayList<Observation>[] npcPositions = stateObs.getNPCPositions(stateObs.getAvatarPosition());
         ArrayList<Observation>[] resourcePositions = stateObs.getResourcesPositions(stateObs.getAvatarPosition());
-        // ArrayList<Observation>[] immovablePositions = stateObs.getImmovablePositions(stateObs.getAvatarPosition());
+        ArrayList<Observation>[] immovablePositions = stateObs.getImmovablePositions(stateObs.getAvatarPosition());
         Vector2d originalPos = previousObs.getAvatarPosition();
         Vector2d currentPos = stateObs.getAvatarPosition();
         double speed = stateObs.getAvatarSpeed();
-        // boolean isnpc = false;
-        // boolean isresource = false;
+        boolean isnpc = false;
+        boolean isresource = false;
 
         if(!gameOver)
         {
@@ -44,36 +44,36 @@ public class SimplestHeuristic {
             {
                 closestResource = resourcePositions[0].get(0).sqDist/stateObs.getBlockSize();
                 score += 1/(closestResource/20);
-                // isresource = true;
+                isresource = true;
             } 
             else if (npcPositions != null)
             {
                 closestNPC = npcPositions[0].get(0).sqDist/stateObs.getBlockSize();
-                score += Math.ceil(1/(closestNPC/50));
-                // isnpc = true;
+                score += Math.ceil(1/(closestNPC/20));
+                isnpc = true;
             }
 
             // one attempt to fix getting stuck
-            // if((immovablePositions != null) && (immovablePositions[0].size()>0))
-            // {
-            //     closestImmovable = npcPositions[0].get(0).sqDist/stateObs.getBlockSize();
+            if((immovablePositions != null) && (immovablePositions[0].size()>0))
+            {
+                closestImmovable = npcPositions[0].get(0).sqDist/stateObs.getBlockSize();
 
-            //     if(isresource)
-            //     {
-            //         if (closestImmovable < closestResource)
-            //         {
-            //             score -= (1/(closestResource/20))/2;
-            //         }
-            //     }
+                if(isresource)
+                {
+                    if (closestImmovable < closestResource)
+                    {
+                        score -= (1/(closestResource/20))/2;
+                    }
+                }
 
-            //     if(isnpc)
-            //     {
-            //         if (closestImmovable < closestNPC)
-            //         {
-            //             score -= Math.ceil(1/(closestNPC/50))/2;
-            //         }
-            //     }
-            // }
+                if(isnpc)
+                {
+                    if (closestImmovable < closestNPC)
+                    {
+                        score -= Math.ceil(1/(closestNPC/50))/2;
+                    }
+                }
+            }
 
         }
 
