a757f6789ffe9de015ae3392409084df0ca9a3eb
Seb Kilmartin <sebastian.kilmartin@gmail.com>
2021-09-25 22:29:45 +0930

    shoudnt get stuck...?

--- src/tracks/singlePlayer/tools/Heuristics/SimplestHeuristic.java
+++ src/tracks/singlePlayer/tools/Heuristics/SimplestHeuristic.java
@@ -17,7 +17,7 @@ public class SimplestHeuristic {
 
     }
 
-    public double evaluateState(StateObservation stateObs) {
+    public double evaluateState(StateObservation stateObs, StateObservation previousObs) {
 
         boolean gameOver = stateObs.isGameOver();
         Types.WINNER win = stateObs.getGameWinner();
@@ -32,6 +32,8 @@ public class SimplestHeuristic {
         ArrayList<Observation>[] npcPositions = stateObs.getNPCPositions(stateObs.getAvatarPosition());
         ArrayList<Observation>[] resourcePositions = stateObs.getResourcesPositions(stateObs.getAvatarPosition());
         ArrayList<Observation>[] immovablePositions = stateObs.getImmovablePositions(stateObs.getAvatarPosition());
+        Vector2d originalPos = previousObs.getAvatarPosition();
+        Vector2d currentPos = stateObs.getAvatarPosition();
         boolean isnpc = false;
         boolean isresource = false;
 
@@ -50,31 +52,34 @@ public class SimplestHeuristic {
                 isnpc = true;
             }
 
-            if((immovablePositions != null) && (immovablePositions[0].size()>0))
-            {
-                closestImmovable = npcPositions[0].get(0).sqDist/stateObs.getBlockSize();
+            // if((immovablePositions != null) && (immovablePositions[0].size()>0))
+            // {
+            //     closestImmovable = npcPositions[0].get(0).sqDist/stateObs.getBlockSize();
 
-                if(isresource)
-                {
-                    if (closestImmovable < closestResource)
-                    {
-                        score -= (1/(closestResource/20))/2;
-                    }
-                }
+            //     if(isresource)
+            //     {
+            //         if (closestImmovable < closestResource)
+            //         {
+            //             score -= (1/(closestResource/20))/2;
+            //         }
+            //     }
 
-                if(isnpc)
-                {
-                    if (closestImmovable < closestNPC)
-                    {
-                        score -= Math.ceil(1/(closestNPC/50))/2;
-                    }
-                }
-            }
+            //     if(isnpc)
+            //     {
+            //         if (closestImmovable < closestNPC)
+            //         {
+            //             score -= Math.ceil(1/(closestNPC/50))/2;
+            //         }
+            //     }
+            // }
 
         }
 
         // if(gameOver && (stateObs.getGameTick()<1400))
         //     return -1000000;
+
+        if (currentPos == originalPos)
+            return 0;
 
         if(gameOver && win == Types.WINNER.PLAYER_LOSES)
             return 0;
