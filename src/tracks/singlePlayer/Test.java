826332a0274cb4976a55972641c95b44f335bafa
Fouad Nehme <fouad-nehme@outlook.com>
2021-09-25 10:12:22 +0930

    merge

--- src/tracks/singlePlayer/Test.java
+++ src/tracks/singlePlayer/Test.java
@@ -37,7 +37,7 @@ public class Test {
 		int seed = new Random().nextInt();
 
 		// Game and level to play
-		int gameIdx = 13; 
+		int gameIdx = 0; 
 		int levelIdx = 0; // level names from 0 to 4 (game_lvlN.txt).
 		String gameName = games[gameIdx][1];
 		String game = games[gameIdx][0];
@@ -53,7 +53,7 @@ public class Test {
 
 		// 2. This plays a game in a level by the controller.
 		//ArcadeMachine.runOneGame(game, level1, visuals, sampleOneStepController, recordActionsFile, seed, 0);
-		ArcadeMachine.runOneGame(game, level1, visuals, diyGA, recordActionsFile, seed, 0);
+		ArcadeMachine.runOneGame(game, level1, visuals, sampleGAController, recordActionsFile, seed, 0);
 
 
 		// 3. This replays a game from an action file previously recorded
