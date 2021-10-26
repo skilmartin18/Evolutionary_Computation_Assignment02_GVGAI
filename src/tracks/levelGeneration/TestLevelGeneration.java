06a19d0deb6a006dac6aa8d09b4a223d567ac6a6
Seb Kilmartin <sebastian.kilmartin@gmail.com>
2021-10-26 12:26:07 +1030

    creating seed individuals finished -SEB

--- src/tracks/levelGeneration/TestLevelGeneration.java
+++ src/tracks/levelGeneration/TestLevelGeneration.java
@@ -11,31 +11,13 @@ public class TestLevelGeneration {
 		String randomLevelGenerator = "tracks.levelGeneration.randomLevelGenerator.LevelGenerator";
 		String geneticGenerator = "tracks.levelGeneration.geneticLevelGenerator.LevelGenerator";
 		String constructiveLevelGenerator = "tracks.levelGeneration.constructiveLevelGenerator.LevelGenerator";
-
+		String diyLevelGenerator = "evo_exercises.Assignment3.Ex4_levelgen.LevelGenerator";
 		String gamesPath = "examples/gridphysics/";
 		String physicsGamesPath = "examples/contphysics/";
 		String generateLevelPath = gamesPath;
 
 
-		String games[] = new String[] { "aliens", "angelsdemons", "assemblyline", "avoidgeorge", "bait", // 0-4
-				"beltmanager", "blacksmoke", "boloadventures", "bomber", "bomberman", // 5-9
-				"boulderchase", "boulderdash", "brainman", "butterflies", "cakybaky", // 10-14
-				"camelRace", "catapults", "chainreaction", "chase", "chipschallenge", // 15-19
-				"clusters", "colourescape", "chopper", "cookmepasta", "cops", // 20-24
-				"crossfire", "defem", "defender", "digdug", "dungeon", // 25-29
-				"eighthpassenger", "eggomania", "enemycitadel", "escape", "factorymanager", // 30-34
-				"firecaster", "fireman", "firestorms", "freeway", "frogs", // 35-39
-				"garbagecollector", "gymkhana", "hungrybirds", "iceandfire", "ikaruga", // 40-44
-				"infection", "intersection", "islands", "jaws", "killBillVol1", // 45-49
-				"labyrinth", "labyrinthdual", "lasers", "lasers2", "lemmings", // 50-54
-				"missilecommand", "modality", "overload", "pacman", "painter", // 55-59
-				"pokemon", "plants", "plaqueattack", "portals", "raceBet", // 60-64
-				"raceBet2", "realportals", "realsokoban", "rivers", "roadfighter", // 65-69
-				"roguelike", "run", "seaquest", "sheriff", "shipwreck", // 70-74
-				"sokoban", "solarfox", "superman", "surround", "survivezombies", // 75-79
-				"tercio", "thecitadel", "thesnowman", "waitforbreakfast", "watergame", // 80-84
-				"waves", "whackamole", "wildgunman", "witnessprotection", "wrapsokoban", // 85-89
-				"zelda", "zenpuzzle"}; //90, 91
+		String games[] = new String[] { "cookmepasta"}; 
 
 
 		String recordActionsFile = null;// "actions_" + games[gameIdx] + "_lvl"
@@ -45,13 +27,13 @@ public class TestLevelGeneration {
 
 		// Other settings
 		int seed = new Random().nextInt();
-		int gameIdx = 11;
+		int gameIdx = 0;
 		String recordLevelFile = generateLevelPath + games[gameIdx] + "_glvl.txt";
 		String game = generateLevelPath + games[gameIdx] + ".txt";
 
 
 		// 1. This starts a game, in a generated level created by a specific level generator
-		if(LevelGenMachine.generateOneLevel(game, constructiveLevelGenerator, recordLevelFile)){
+		if(LevelGenMachine.generateOneLevel(game, diyLevelGenerator, recordLevelFile)){
 		    LevelGenMachine.playOneGeneratedLevel(game, recordActionsFile, recordLevelFile, seed);
 		}
 
